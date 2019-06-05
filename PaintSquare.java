eaa6483693d9390479b628ff83539dd4d429e702
Sweetie77 <1443354525@qq.com>
2019-06-05 22:31:55 +0800

    PaintSquare.java

---
+++ PaintSquare.java
@@ -0,0 +1,20 @@
+package day13;
+
+/**
+ * Author: Sweetie77
+ * Created: 2019/6/3
+ */
+public class PaintSquare {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        int n = 0;
+        char ch = 0;
+        for(i = 1; i <=n;i++){
+            for(j = 1; j <= n;j++){
+                System.out.println(ch);
+            }
+            System.out.println();
+        }
+    }
+}
