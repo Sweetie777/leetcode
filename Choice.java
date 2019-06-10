163eff398c665f881b96ab8be000e415ba8daa76
Sweetie77 <1443354525@qq.com>
2019-06-10 22:48:04 +0800

    day16 Choice

---
+++ Choice.java
@@ -0,0 +1,19 @@
+package day16;
+
+/**
+ * Author: Sweetie77
+ * Created: 2019/6/6
+ */
+public class Choice {
+    public static void main(String[] args) {
+//        int i = 5;
+//        int s = (i++)+(++i)+(i--)+(--i);
+//        System.out.println(s);
+        int i = 0;
+        int j = 0;
+        if((++i>0)||(++j>0)){
+            System.out.println(i);
+            System.out.println(j);
+        }
+    }
+}
