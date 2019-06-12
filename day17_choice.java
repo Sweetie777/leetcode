affe6aeaab6be1051191d8aa2a1376237857db61
Sweetie77 <1443354525@qq.com>
2019-06-12 16:36:40 +0800

    day17_choice

---
+++ day17_choice.java
@@ -0,0 +1,13 @@
+package day17;
+
+/**
+ * Author: Sweetie77
+ * Created: 2019/6/8
+ */
+public class day17_choice {
+    static boolean Paddy;
+
+    public static void main(String[] args) {
+        System.out.println(Paddy);
+    }
+}
